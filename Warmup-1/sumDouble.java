44597853624e7cf3343b7079188f643e54a7d318
RaniaMesak <97768669+RaniaMesak@users.noreply.github.com>
2022-01-27 21:31:48 -0600

    Create sumDouble.java

---
+++ Warmup-1/sumDouble.java
@@ -0,0 +1,11 @@
+public int sumDouble(int firstNumber, int secondNumber)
+{
+  int sum = firstNumber + secondNumber;
+  
+  if (firstNumber == secondNumber)
+  {
+    sum = sum * 2;
+  }
+  
+  return sum;
+}
